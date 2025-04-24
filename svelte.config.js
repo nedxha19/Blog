7468fdd1fd5c7a43a5404de7d5d3216aa0ee4a75
Nedit <nedxha19@htl-shkoder.com>
2025-04-24 16:32:46 +0200

    Fix SSR build: mark @vercel/blob as noExternal in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			ssr: {
+				noExternal: ['@vercel/blob']
+			}
+		}
 	}
 };
 
