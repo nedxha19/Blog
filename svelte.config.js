28676b3ff04c1b64cd263271097745da355e61a1
Nedit <nedxha19@htl-shkoder.com>
2025-04-24 16:42:17 +0200

    Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    vite: {
-      ssr: {
-        noExternal: ['@vercel/blob']
-      }
-    }
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
